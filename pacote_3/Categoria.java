edca12d524a56ceadd5fb21f4f00eb60b8b57ff4
João Pedro Plinta <joaopedrohenriqueplinta@gmail.com>
2024-01-18 14:32:20 -0300

    Implementação do CRUD

--- pacote_3/Categoria.java
+++ pacote_3/Categoria.java
@@ -1,15 +1,20 @@
 package pacote_3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Categoria {
     private int ID;
     private String nome;
     private String descricao;
+    private static List<Categoria> listaDeCategorias;
 
     // Construtor
     public Categoria(int ID, String nome, String descricao) {
         this.ID = ID;
         this.nome = nome;
         this.descricao = descricao;
+        listaDeCategorias = new ArrayList<>();
     }
 
     // Getters and Setters
@@ -42,20 +47,46 @@ public class Categoria {
     }
 
     // CRUD methods
-    public void criar() {
+    public void criar(int novoID, String novoNome, String novaDescricao) {
         // Implementação para criar um novo objeto Categoria
+        Categoria novaCategoria = new Categoria(novoID, novoNome, novaDescricao);
+        listaDeCategorias.add(novaCategoria);
+
     }
 
     public Categoria ler(int ID) {
         // Implementação para ler um objeto Categoria pelo ID
+        for (Categoria categoria : listaDeCategorias) {
+            if (categoria.getID() == ID) {
+                return categoria;
+            }
+        }
         return null;
     }
 
-    public void atualizar() {
+    public void atualizar(int ID, String novoNome, String novaDescricao) {
         // Implementação para atualizar um objeto Categoria existente
+        for (Categoria categoria : listaDeCategorias) {
+            if (categoria.getID() == ID) {
+                categoria.setNome(novoNome);
+                categoria.setDescricao(novaDescricao);
+
+                break;
+            }
+        }
     }
 
-    public void deletar() {
+    public void deletar(int ID) {
         // Implementação para deletar um objeto Categoria existente
+        Categoria categoriaParaRemover = null;
+        for (Categoria categoria : listaDeCategorias) {
+            if (categoria.getID() == ID) {
+                categoriaParaRemover = categoria;
+                break;
+            }
+        }
+        if (categoriaParaRemover != null) {
+            listaDeCategorias.remove(categoriaParaRemover);
+        }
     }
 }
