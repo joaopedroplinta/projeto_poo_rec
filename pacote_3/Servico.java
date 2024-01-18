92b600823214a5e601f909261c8d12fec58cd18f
João Pedro Plinta <joaopedrohenriqueplinta@gmail.com>
2024-01-18 14:21:19 -0300

    Implementando o CRUD

--- pacote_3/Servico.java
+++ pacote_3/Servico.java
@@ -1,15 +1,19 @@
 package pacote_3;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Servico {
     private int ID;
     private String descricao;
     private double preco;
+    private static List<Servico> listaDeServicos;
 
     // Constructor
     public Servico(int ID, String descricao, double preco) {
         this.ID = ID;
         this.descricao = descricao;
         this.preco = preco;
+        listaDeServicos = new ArrayList<>();
     }
 
     // Getters and Setters
@@ -38,20 +42,45 @@ public class Servico {
     }
 
     // CRUD metodos
-    public void criar() {
+    public void criar(int novoID, String novaDescricao, double novoPreco) {
         // Implementação para criar um novo objeto Servico
+        Servico novoServico = new Servico(novoID, novaDescricao, novoPreco);
+        listaDeServicos.add(novoServico);
     }
 
     public Servico ler(int ID) {
         // Implementação para ler um objeto Servico pelo ID
+        for (Servico servico : listaDeServicos) {
+            if (servico.getID() == ID) {
+                return servico;
+            }
+        }
         return null;
     }
 
-    public void atualizar() {
+    public void atualizar(int ID, String novaDescricao, double novoPreco) {
         // Implementação para atualizar um objeto Servico existente
+        for (Servico servico : listaDeServicos) {
+            if (servico.getID() == ID) {
+                servico.setDescricao(novaDescricao);
+                servico.setPreco(novoPreco);
+
+                break;
+            }
+        }
     }
 
-    public void deletar() {
+    public void deletar(int ID) {
         // Implementação para deletar um objeto Servico existente
+        Servico servicoParaRemover = null;
+        for (Servico servico : listaDeServicos) {
+            if (servico.getID() == ID) {
+                servicoParaRemover = servico;
+                break;
+            }
+        }
+        if (servicoParaRemover != null) {
+            listaDeServicos.remove(servicoParaRemover);
+        }
     }
 }
