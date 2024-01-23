51d9db49386240049b1911de3cd9b969aa5ff847
karoline lima sousa <141361933+klkarolinelima@users.noreply.github.com>
2024-01-23 20:44:33 -0300

    Update Usuario.java

--- pacote_1/Usuario.java
+++ pacote_1/Usuario.java
@@ -1,5 +1,50 @@
 package pacote_1;
 
 public class Usuario {
-    
+    private int ID;
+    private String CPF;
+    private String nome;
+    private int telefone;
+    private String endereco;
+
+    public Usuario(int ID, String CPF, String nome, int telefone, String endereco){
+        this.ID = ID;
+        this.CPF = CPF;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
+  // Getters and Setters são usados para proteger seus dados, especialmente na criação de classes.
+  public int getID() {
+    return ID;
+}
+
+public String getNome() {
+    return nome;
+}
+
+public String getEndereco() {
+    return endereco;
+}
+
+public int getTelefone() {
+    return this.telefone;
+}
+
+public void setID(int ID) {
+    this.ID = ID;
+}
+
+public void setNome(String nome) {
+    this.nome = nome;
+}
+
+public void setEndereco(String endereco) {
+    this.endereco = endereco;
+}
+
+public void setTelefone(String telefone) {
+    this.telefone = telefone;
+}
+
 }
