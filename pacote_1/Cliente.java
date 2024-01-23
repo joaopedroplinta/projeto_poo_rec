d071227e4075fd5488fe877e8677f6e953653c6d
João Pedro Plinta <joaopedrohenriqueplinta@gmail.com>
2024-01-23 20:57:01 -0300

    Implementando o CRUD

--- pacote_1/Cliente.java
+++ pacote_1/Cliente.java
@@ -2,22 +2,14 @@ package pacote_1;
 
 import java.util.List;
 
-public class Cliente {
-    private int ID;
-    private String nome;
-    private String endereco;
-    private String telefone;
+public class Cliente extends Usuario{
 
-
-    public Cliente(int ID, String nome, String endereco, String telefone) {
-        this.ID = ID;
-        this.nome = nome;
-        this.endereco = endereco;
-        this.telefone = telefone;
+    public Cliente(int ID, String CPF, String nome, String endereco, int telefone) {
+        super(ID,CPF,nome,telefone,endereco);
     }
 
-    public static Cliente criarCliente(int ID, String nome, String endereco, String telefone) {
-        return new Cliente(ID, nome, endereco, telefone);
+    public static Cliente criarCliente(int ID, String CPF, String nome, String endereco, int telefone) {
+        return new Cliente(ID, CPF, nome, endereco, telefone);
     }
 
     public static Cliente lerCliente(int ID, List<Cliente> clientes) {
@@ -41,38 +33,5 @@ public class Cliente {
 
     public static void excluirCliente(int ID, List<Cliente> clientes) {
         clientes.removeIf(cliente -> cliente.getID() == ID);
-    }
-
-    // Getters and Setters são usados para proteger seus dados, especialmente na criação de classes.
-    public int getID() {
-        return ID;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public int setID(int ID) {
-        return this.ID = ID;
-    }
-
-    public String setNome(String nome) {
-        return this.nome = nome;
-    }
-
-    public String setEndereco(String endereco) {
-        return this.endereco = endereco;
-    }
-
-    public String setTelefone(String telefone) {
-        return this.telefone = telefone;
     }
 }
