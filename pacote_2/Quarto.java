03ed7197d3efb6c88a53f421442f7737d268169d
João Pedro Plinta <joaopedrohenriqueplinta@gmail.com>
2024-01-24 19:33:56 -0300

    Implementando o CRUD

--- pacote_2/Quarto.java
+++ pacote_2/Quarto.java
@@ -1,15 +1,20 @@
 package pacote_2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Quarto {
     private int numero;
     private String categoria;
     private String status;
+    private List<Quarto> quartos;
 
     // Construtor
     public Quarto(int numero, String categoria, String status) {
         this.numero = numero;
         this.categoria = categoria;
         this.status = status;
+        this.quartos = new ArrayList<>();
     }
 
     // Getters e Setters
@@ -27,5 +32,34 @@ public class Quarto {
 
     public void setStatus(String novoStatus) {
         this.status = novoStatus;
+    }
+
+     // Métodos CRUD para Quarto
+    public void criarQuarto(Quarto quarto) {
+        quartos.add(quarto);
+    }
+
+    public Quarto lerQuarto(int numero) {
+        for (Quarto quarto : quartos) {
+            if (quarto.getNumero() == numero) {
+                return quarto;
+            }
+        }
+        return null;
+    }
+
+    public void atualizarQuarto(int numero, Quarto novosDados) {
+        Quarto quarto = lerQuarto(numero);
+        if (quarto != null) {
+            quarto.setStatus(novosDados.getStatus());
+        }
+    }
+
+
+    public void deletarQuarto(int numero) {
+        Quarto quarto = lerQuarto(numero);
+        if (quarto != null) {
+            quartos.remove(quarto);
+        }
     }
 }
