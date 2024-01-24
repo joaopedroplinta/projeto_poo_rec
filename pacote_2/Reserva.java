b055a6394f71e8a48511abdb5b536775a321d933
João Pedro Plinta <joaopedrohenriqueplinta@gmail.com>
2024-01-24 19:21:18 -0300

    Update

--- pacote_2/Reserva.java
+++ pacote_2/Reserva.java
@@ -1,5 +1,49 @@
 package pacote_2;
 
 public class Reserva {
-    
+    private int id;
+    private String cliente;
+    private Quarto quarto;
+    private String dataCheckIn;
+    private String dataCheckOut;
+    private double valorTotal;
+
+    // Construtor
+    public Reserva(int id, String cliente, Quarto quarto, String dataCheckIn, String dataCheckOut, double valorTotal) {
+        this.id = id;
+        this.cliente = cliente;
+        this.quarto = quarto;
+        this.dataCheckIn = dataCheckIn;
+        this.dataCheckOut = dataCheckOut;
+        this.valorTotal = valorTotal;
+    }
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public Quarto getQuarto() {
+        return quarto;
+    }
+
+    public String getDataCheckIn() {
+        return dataCheckIn;
+    }
+
+    public String getDataCheckOut() {
+        return dataCheckOut;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(double novoValorTotal) {
+        this.valorTotal = novoValorTotal;
+    }
 }
