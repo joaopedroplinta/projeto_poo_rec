fb58e43ba35b069f7042c4782a5d0e71de2756cd
João Pedro Plinta <joaopedrohenriqueplinta@gmail.com>
2024-01-24 19:18:33 -0300

    Criando o Quarto

--- pacote_2/Quarto.java
+++ pacote_2/Quarto.java
@@ -1,5 +1,31 @@
 package pacote_2;
 
 public class Quarto {
-    
+    private int numero;
+    private String categoria;
+    private String status;
+
+    // Construtor
+    public Quarto(int numero, String categoria, String status) {
+        this.numero = numero;
+        this.categoria = categoria;
+        this.status = status;
+    }
+
+    // Getters e Setters
+    public int getNumero() {
+        return numero;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String novoStatus) {
+        this.status = novoStatus;
+    }
 }
