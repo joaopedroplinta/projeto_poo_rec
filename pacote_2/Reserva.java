130c7cb032d74f9cb8b3193326470b268c1ff684
João Pedro Plinta <joaopedrohenriqueplinta@gmail.com>
2024-01-24 19:30:54 -0300

    Metodo CRUD para Reserva

--- pacote_2/Reserva.java
+++ pacote_2/Reserva.java
@@ -1,5 +1,8 @@
 package pacote_2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Reserva {
     private int id;
     private String cliente;
@@ -7,6 +10,7 @@ public class Reserva {
     private String dataCheckIn;
     private String dataCheckOut;
     private double valorTotal;
+    private List<Reserva> reservas;
 
     // Construtor
     public Reserva(int id, String cliente, Quarto quarto, String dataCheckIn, String dataCheckOut, double valorTotal) {
@@ -16,6 +20,7 @@ public class Reserva {
         this.dataCheckIn = dataCheckIn;
         this.dataCheckOut = dataCheckOut;
         this.valorTotal = valorTotal;
+        this.reservas = new ArrayList<>();
     }
 
     // Getters e Setters
@@ -45,5 +50,33 @@ public class Reserva {
 
     public void setValorTotal(double novoValorTotal) {
         this.valorTotal = novoValorTotal;
+    }
+
+     // Métodos CRUD para Reserva
+    public void criarReserva(Reserva reserva) {
+        reservas.add(reserva);
+    }
+
+    public Reserva lerReserva(int id) {
+        for (Reserva reserva : reservas) {
+            if (reserva.getId() == id) {
+                return reserva;
+            }
+        }
+        return null;
+    }
+
+    public void atualizarReserva(int id, Reserva novosDados) {
+        Reserva reserva = lerReserva(id);
+        if (reserva != null) {
+            reserva.setValorTotal(novosDados.getValorTotal());
+        }
+    }
+
+    public void deletarReserva(int id) {
+        Reserva reserva = lerReserva(id);
+        if (reserva != null) {
+            reservas.remove(reserva);
+        }
     }
 }
