4eae213540648eb804a7927fddb82665eddf657d
João Pedro Plinta <joaopedrohenriqueplinta@gmail.com>
2024-01-29 20:26:45 -0300

    Updates

--- pacote_1/Usuario.java
+++ pacote_1/Usuario.java
@@ -1,11 +1,5 @@
 package pacote_1;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 public class Usuario {
     private int ID;
     private String CPF;
