2581227b7b926b7f409a9be6b2ce0edba0c06882
kl.karoline <141361933+klkarolinelima@users.noreply.github.com>
2024-01-29 23:26:08 -0300

    Update SistemaHotel.java
    atualização

--- SistemaHotel.java
+++ SistemaHotel.java
@@ -4,26 +4,11 @@ import pacote_3.*;
 import java.util.Scanner;
 import java.io.*;
 
+
 public class SistemaHotel {
     private static Scanner scanner = new Scanner(System.in);
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_AMARELO = "\u001B[33m";
-    public static final String ANSI_AZUl = "\u001B[34m";
-    public static final String ANSI_VERMELHO = "\u001B[31m";
-
-    private static void limparTerminal() {
-        try {
-            String os = System.getProperty("os.name").toLowerCase();
-
-            if (os.contains("win")) {
-                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-            } else {
-                new ProcessBuilder("clear").inheritIO().start().waitFor();
-            }
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+    private static Cliente clienteAtual;
+    private static Funcionario funcionarioAtual;
 
     public static void main(String[] args) {
         exibirMenuPrincipal();
@@ -38,14 +23,14 @@ public class SistemaHotel {
         System.out.print("Escolha uma opção: ");
 
         int opcao = scanner.nextInt();
-        scanner.nextLine(); // Consumir a quebra de linha
+        scanner.nextLine(); 
 
         switch (opcao) {
             case 1:
-                menuCliente();
+                loginCliente();
                 break;
             case 2:
-                menuFuncionario();
+                loginFuncionario();
                 break;
             case 3:
                 System.out.println("Sistema encerrado. Até logo!");
@@ -56,27 +41,36 @@ public class SistemaHotel {
         }
     }
 
+    private static void loginCliente() {
+        clienteAtual = new Cliente(1, "123456789", "Cliente Teste", "Endereco Teste", 987654321);
+        menuCliente();
+    }
+
+    private static void loginFuncionario() {
+        funcionarioAtual = new Funcionario(1, "987654321", "Funcionario Teste", 123456789, "Endereco Funcionario");
+        menuFuncionario();
+    }
+
     private static void menuCliente() {
         limparTerminal();
         System.out.println(ANSI_AMARELO + "Menu do Cliente" + ANSI_RESET);
-        // Adicione as opções específicas para o cliente aqui
-        System.out.println(ANSI_AZUl + "1. Cadastrar Cliente" + ANSI_RESET);
-        System.out.println(ANSI_AZUl + "2. Realizar Reserva" + ANSI_RESET);
-        System.out.println(ANSI_AZUl + "3. Consultar Reservas" + ANSI_RESET);
+        System.out.println(ANSI_AZUl + "1. Realizar Reserva" + ANSI_RESET);
+        System.out.println(ANSI_AZUl + "2. Consultar Reservas" + ANSI_RESET);
         System.out.println(ANSI_VERMELHO + "-1. Voltar ao Menu Principal" + ANSI_RESET);
         System.out.print("Escolha uma opção: ");
 
         int opcao = scanner.nextInt();
-        scanner.nextLine(); // Consumir a quebra de linha
+        scanner.nextLine(); 
 
         switch (opcao) {
             case 1:
-                System.out.println("Implemente a lógica para realizar reserva aqui.");
+                realizarReserva();
                 break;
             case 2:
-                System.out.println("Implemente a lógica para consultar reservas aqui.");
+                consultarReservas();
                 break;
             case -1:
+                clienteAtual = null;
                 exibirMenuPrincipal();
                 break;
             default:
@@ -85,10 +79,17 @@ public class SistemaHotel {
         }
     }
 
+    private static void realizarReserva() {
+        System.out.println("Implemente a lógica para realizar reserva aqui.");
+    }
+
+    private static void consultarReservas() {
+        System.out.println("Implemente a lógica para consultar reservas aqui.");
+    }
+
     private static void menuFuncionario() {
         limparTerminal();
         System.out.println(ANSI_AMARELO + "Menu do Funcionário" + ANSI_RESET);
-        // Adicione as opções específicas para o funcionário aqui
         System.out.println(ANSI_AZUl + "1. Adicionar Cliente" + ANSI_RESET);
         System.out.println(ANSI_AZUl + "2. Adicionar Reserva" + ANSI_RESET);
         System.out.println(ANSI_AZUl + "3. Adicionar Quartos" + ANSI_RESET);
@@ -99,28 +100,29 @@ public class SistemaHotel {
         System.out.print("Escolha uma opção: ");
 
         int opcao = scanner.nextInt();
-        scanner.nextLine(); // Consumir a quebra de linha
+        scanner.nextLine();
 
         switch (opcao) {
             case 1:
-                System.out.println("Implemente a lógica para adicionar cliente aqui.");
-                break;                
+                adicionarCliente();
+                break;
             case 2:
-                System.out.println("Implemente a lógica para adicionar reserva aqui.");
+                adicionarReserva();
                 break;
             case 3:
-                System.out.println("Implemente a lógica para adicionar quartos aqui.");
+                adicionarQuartos();
                 break;
             case 4:
-                System.out.println("Implemente a lógica para consultar clientes aqui.");
+                consultarClientes();
                 break;
             case 5:
-                System.out.println("Implemente a lógica para consultar reservas aqui.");
+                consultarReservas();
                 break;
             case 6:
-                System.out.println("Implemente a lógica para consultar quartos aqui.");
+                consultarQuartos();
                 break;
             case -1:
+                funcionarioAtual = null;
                 exibirMenuPrincipal();
                 break;
             default:
@@ -128,4 +130,47 @@ public class SistemaHotel {
                 menuFuncionario();
         }
     }
+
+    private static void adicionarCliente() {
+        System.out.println("Implemente a lógica para adicionar cliente aqui.");
+    }
+
+    private static void adicionarReserva() {
+        System.out.println("Implemente a lógica para adicionar reserva aqui.");
+    }
+
+    private static void adicionarQuartos() {
+        System.out.println("Implemente a lógica para adicionar quartos aqui.");
+    }
+
+    private static void consultarClientes() {
+        System.out.println("Implemente a lógica para consultar clientes aqui.");
+    }
+
+    private static void consultarQuartos() {
+        System.out.println("Implemente a lógica para consultar quartos aqui.");
+    }
+
+    private static void consultarReservas() {
+        System.out.println("Implemente a lógica para consultar reservas aqui.");
+    }
+
+    private static void limparTerminal() {
+        try {
+            String os = System.getProperty("os.name").toLowerCase();
+
+            if (os.contains("win")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_AMARELO = "\u001B[33m";
+    public static final String ANSI_AZUl = "\u001B[34m";
+    public static final String ANSI_VERMELHO = "\u001B[31m";
 }
